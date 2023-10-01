f69929e131f2f3fc0c497f03e30b0825be4a47be
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-01 16:37:05 +0200

    refactor(ex00): cleanup

--- ex00/statistics.py
+++ ex00/statistics.py
@@ -1,60 +1,54 @@
-import numpy
-
-def ft_mean(l: list):
-    return sum(l) / len(l)
+def ft_mean(pop: list):
+    return sum(pop) / len(pop)
 
 
-def ft_median(l: list):
-    l.sort()
-    n = len(l)
+def ft_median(pop: list):
+    pop.sort()
+    n = len(pop)
     middleIndex = n // 2
     if n % 2:
-        return l[middleIndex]
+        return pop[middleIndex]
     else:
-        return (l[middleIndex - 1] + l[middleIndex]) / 2
+        return (pop[middleIndex - 1] + pop[middleIndex]) / 2
 
 
-def ft_quartiles(l: list):
-    l.sort()
-    i1 = len(l) // 4
+def ft_quartiles(pop: list):
+    pop.sort()
+    i1 = len(pop) // 4
     i3 = i1 * 3
-    q1 = l[i1]
-    q3 = l[i3]
+    q1 = pop[i1]
+    q3 = pop[i3]
     return [float(q1), float(q3)]
 
 
-def ft_variance(l):
-    mean = ft_mean(l)
-    tot = sum((x - mean) ** 2 for x in l)
-    n = len(l)
-    variance = tot / n
-    return(variance)
+def ft_variance(pop):
+    mean = ft_mean(pop)
+    tot = sum((x - mean) ** 2 for x in pop)
+    return (tot / len(pop))
 
 
-def ft_standard_deviation(l):
-    variance = ft_variance(l)
-    std = variance ** 0.5
-    return(std)
+def ft_standard_deviation(pop):
+    return ft_variance(pop) ** 0.5
 
 
-def ft_statistics(*args: any, **kwargs: any) -> None:
+def ft_statistics(*args, **kwargs) -> None:
     if not args:
-        print("ERROR")
-        print("ERROR")
-        print("ERROR")
+        for _ in range(3):
+            print("ERROR")
         return
-    l = list(args)
-    for key, value in kwargs.items():
-        if value == 'mean':
-            print(f'mean : {ft_mean(l)}')
-        elif value == 'median':
-            print(f'median : {ft_median(l)}')
-        elif value == 'quartile':
-            print(f'quartile : {ft_quartiles(l)}')
-        elif value == 'std':
-            print(f'std : {ft_standard_deviation(l)}')
-        elif value == 'var':
-            print(f'var : {ft_variance(l)}')
+
+    pop = list(args)
+    statistics_map = {
+        'mean': ft_mean,
+        'median': ft_median,
+        'quartile': ft_quartiles,
+        'std': ft_standard_deviation,
+        'var': ft_variance
+    }
+
+    for _, value in kwargs.items():
+        func = statistics_map.get(value)
+        if func:
+            print(f'{value} : {func(pop)}')
         else:
             return
-
